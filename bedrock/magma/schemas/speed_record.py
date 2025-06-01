66cd998f40c8cdf5c7cd0e714f9737ac47c270cd
Jimmy Gizmo <1915982+jimmygizmo@users.noreply.github.com>
2025-05-31 19:51:54 -0700

    notebook now connecting to backend. does not yet get secrets/config from env but that is not critical. notebook defaults to dark theme. noice.

--- bedrock/magma/schemas/speed_record.py
+++ bedrock/magma/schemas/speed_record.py
@@ -16,8 +16,9 @@ class SpeedRecordBase(BaseModel):
 # -------- Used for incoming POST data (example: new LinkStringGeometry) --------
 class SpeedRecordCreate(SpeedRecordBase):
 
-    # TODO: Elsewhere in Bedrock, change to using validate_by_name and validate_by_aliase over populate_by_name
+    # TODO: Elsewhere in Bedrock, change to using validate_by_name and validate_by_alias over populate_by_name
     model_config = ConfigDict(extra="forbid", validate_by_name=True, validate_by_alias=True)
+
 
 # -------- Used for response serialization (example: API GET /links/1) --------
 class SpeedRecordRead(SpeedRecordBase):
